388bf6be5887f38feea9bde9ce69bcacbd1c25d9
smilecamo <smilecamo99@gmail.com>
2019-03-04 11:08:37 +0800

    数据库

--- expressDemo/routerdemo.js
+++ expressDemo/routerdemo.js
@@ -1,12 +1,57 @@
 var express = require('express')
-
+const mongoose = require('mongoose')
+// 定义mongoose地址
+const DB_URL = 'mongodb://localhost:27017/nodeDemo'
+// 链接数据库
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true
+})
+// 链接成功后打印成功信息进行提示
+mongoose.connection.on('connected',function(){
+  console.log('mongo connect success');
+})
+// 建立表
+const User = mongoose.model('user', new mongoose.Schema({
+  name: {type: String,required: true},
+  age: {type: Number,required: true}
+}))
 var app = express()
 // 路由中间件
 var index = require('./routes/index')
 var user = require('./routes/user')
 app.use('/index', index)
 app.use('/user', user)
-
+// 新建数据
+// User.create({
+//   name: 'wangwu',
+//   age: 14
+// })
+// 删除数据
+// User.remove({name:'zhangsan'},(req,res)=>{
+//   console.log(res);
+// })
+// 改变数据
+// User.update({name: 'lisi'},{name:'lisilisi',age: 28},(req,res)=>{
+//   console.log(res);
+// })
+// 查找所有符合条件的数据
+app.get('/',(req,res)=>{
+  User.find({
+    name: 'lisi'
+  }, (err, dosc) => {
+    res.send(dosc)
+  })
+})
+// 查找单个符合条件的
+app.get('/findone',(req,res)=>{
+  User.findOne({name:'lisi'},(err,dosc)=>{
+    if(!err){
+      res.json(dosc)
+    }else{
+      res.send(err)
+    }
+  })
+})
 app.listen(3000)
 
 console.log('listten port of 3000');
--- vue-node/client/src/views/Index.vue
+++ vue-node/client/src/views/Index.vue
@@ -4,10 +4,12 @@
       <head-nav></head-nav>
     </el-header>
     <el-container>
-      <el-aside>
+      <el-aside style="width:240px">
         <left-aside></left-aside>
       </el-aside>
-      <el-main><router-view></router-view></el-main>
+      <el-main>
+        <router-view></router-view>
+      </el-main>
     </el-container>
 </el-container>
 </template>
