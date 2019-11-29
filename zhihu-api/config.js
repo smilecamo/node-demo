e0dc9ae4d170ae17adb3127184fcac3b528f3fbe
smilecamo <1191126635@qq.com>
2019-11-29 17:31:57 +0800

    连接网络数据库

---
+++ zhihu-api/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  connectionStr:
+    'mongodb+srv://yang:123456789qq@cluster0-znu0b.mongodb.net/test?retryWrites=true&w=majority'
+};
--- zhihu-api/index.js
+++ zhihu-api/index.js
@@ -2,11 +2,21 @@ const Koa = require('koa')
 const badyparser = require('koa-bodyparser')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter');
+const mongoose = require('mongoose');
 const app = new Koa(); // 实例化koa
 const port = 3000; //设置端口号
 // 引入自动化函数
 const routing = require('./routes/index')
-
+const {connectionStr} = require('./config')
+// 链接数据库
+mongoose.connect(
+  connectionStr,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('数据库链接成功');
+  }
+);
+mongoose.connection.on('error',console.error);
 // 异常处理
 app.use(
   error({
